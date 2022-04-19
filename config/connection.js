deb13dcfac25a8e6e149ebe9fe41d810d24ee4b2
Taylor Hernandez <95730939+TayHern@users.noreply.github.com>
2022-04-19 13:42:49 -0700

    add connections to mysql db

--- config/connection.js
+++ config/connection.js
@@ -2,4 +2,14 @@ const mysql = require('mysql2');
 
 require('dotenv').config();
 
-module.exports =
+const connectionProperties = {
+    host: 'localhost',
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
+};
+
+const connection = mysql.createConnection(connectionProperties);
+
+module.exports = { connectionProperties, connection };
