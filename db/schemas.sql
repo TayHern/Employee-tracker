3230813ae328fa65985c522f2c194853e6616a8e
Taylor Hernandez <95730939+TayHern@users.noreply.github.com>
2022-04-19 13:03:27 -0700

    add boilerplate

---
+++ db/schemas.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS employee_status;
+CREATE DATABASE employee_status;
+
+USE employee_status;
+
+CREATE TABLE department (
+
+);
+
+CREATE TABLE role (
+
+);
+
+CREATE TABLE employee (
+
+);
